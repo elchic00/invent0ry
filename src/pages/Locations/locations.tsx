4d0492f4b025253bc742135c4d66e82914b3fe27
Andrew Alagna <aalagna04@gmail.com>
2022-03-28 15:47:10 -0400

    feat: page for all items related to a business

--- src/pages/Locations/locations.tsx
+++ src/pages/Locations/locations.tsx
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import {API} from "../../services/api";
-import { DataGrid } from '@mui/x-data-grid'
+import {DataGrid, GridColumns} from '@mui/x-data-grid'
 
 export const LocationGrid = () => {
     const [tableData, setTableData] = useState([])
-    const columns = [
-        { field: 'name', headerName: 'Name', width: 200},
-        { field: 'street', headerName: 'Street', width: 200 },
-        { field: 'town', headerName: 'Town', width: 200 },
-        { field: 'zip', headerName: 'Zip' , width: 200},
+    const columns: GridColumns = [
+        { field: 'name',  headerName: 'Name', width: 150, },
+        { field: 'street', headerName: 'Street', width: 220 },
+        { field: 'town', headerName: 'Town', width: 150 },
+        { field: 'zip', headerName: 'Zip' , width: 150},
         // { field: 'owner', headerName: 'Owner', width: 200}
     ]
 
     useEffect(() => {
             API.getLocations().then((data)=> JSON.stringify(data)).then((data)=>JSON.parse(data)).then((data)=>setTableData(data))},[])
     return (
-        <div style={{ height: 600, width: '80%' }}>
+        <div style={{ height: 600, width: '66%' }}>
             <DataGrid
+                sx={{
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: "rgba(236,236,236,1)",
+                        color: "rgba(0,0,0,100)",
+                        fontSize: 16,
+                    },
+                    '& .MuiDataGrid-cell:hover': {
+                        color: 'primary.main',
+                    },
+                }}
+                // sx={{ m: 2}}
                 rows={tableData}
                 columns={columns}
                 pageSize={10}
