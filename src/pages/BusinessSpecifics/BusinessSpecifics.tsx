f62288ec033d5275a7f46eebf0501f56bfa8d4af
Andrew Alagna <aalagna04@gmail.com>
2022-03-28 09:10:28 -0400

    refactor:  businessSpecific forms
    organize code and make api calls to DataStore to submit the business form

--- src/pages/BusinessSpecifics/BusinessSpecifics.tsx
+++ src/pages/BusinessSpecifics/BusinessSpecifics.tsx
@@ -1,51 +1,89 @@
-import { FormControl, TextField, Button, MenuItem, Select } from "@mui/material"
-import { useState } from "react"
-import { currencies } from "./currencies"
-import axios from "axios";
-import {DataStore} from "@aws-amplify/datastore";
-import {Business} from "../../models";
-
+import {
+  FormControl,
+  TextField,
+  Button,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import { useState } from "react";
+import { currencies } from "../../constants/currencies";
+import { DataStore } from "@aws-amplify/datastore";
+import { Business } from "../../models";
+import React from "react";
+import { businessType } from "../../interface/models/businessType";
+import { API } from "../../services/api";
+import { sendNotification } from "../../utils/sendNotification";
 
 export const BusinessSpecifics = () => {
+  const [formData, setFormData] = useState<businessType>({
+    name: "",
+    location_name: "",
+    currency: "USD",
+  });
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
 
-    const [businessName, setBusinessName] = useState("")
-    const [locationName, setLocationName] = useState("")
-    const [currency, setCurrency] = useState(currencies.USD)
+  const currencyList = Object.keys(currencies).map((key) => (
+    <MenuItem value={key}>{key}</MenuItem>
+  ));
 
-    const currencyList = Object.keys(currencies).map(key => (<MenuItem value={key}>{key}</MenuItem>))
+  async function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+    setIsDisabled(true);
 
-    async function handleSubmit(){
-        const form_data ={
-                'name': businessName,
-                'location_name': locationName,
-                'currency': currency
-        }
-        await DataStore.save(
-            new Business(form_data)
-        ).then((data)=>console.log(data));
+    try {
+      const result = await API.businessSpecifics(formData);
+
+      setIsDisabled(false);
+    } catch (error) {
+      sendNotification(
+        "Error trying to call the business specifics api",
+        "error"
+      );
+      setIsDisabled(false);
     }
+  }
 
-    return (
-        <FormControl>
-            <TextField  id="standard-basic" variant="standard" sx={{mb:2}}
-                label="Business Name"
-                helperText="Name of your business"
-                onChange={(e) => setBusinessName(e.target.value)}/>
-            <TextField id="standard-basic" variant="standard" sx={{mb:2}}
-                label="Location Name" 
-                helperText="add your business address nickname, H.Q., main, etc"
-                onChange={(e) => setLocationName(e.target.value)}/>
-            <TextField sx={{mb:4}}
-                select
-                label="Currency"
-                value={currency}
-                onChange={(e) => setCurrency(e.target.value)}>
-                {currencyList}
-            </TextField>
-            <Button variant="contained"
-            disabled={!businessName || !locationName || !currency }
-            onClick={handleSubmit}>submit</Button>
-        </FormControl>     
-    )
-}
-//() => console.log({BusinessName, LocationName, currency})
+  function handleChange(e: React.ChangeEvent) {
+    const { name, value } = e.target as HTMLInputElement;
+
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  return (
+    <FormControl>
+      <TextField
+        id="standard-basic"
+        variant="standard"
+        sx={{ mb: 2 }}
+        name="name"
+        label="Business Name"
+        helperText="Name of your business"
+        onChange={handleChange}
+        value={formData.name}
+      />
+      <TextField
+        name="location_name"
+        id="standard-basic"
+        variant="standard"
+        sx={{ mb: 2 }}
+        label="Location Name"
+        helperText="add your business address nickname, H.Q., main, etc"
+        onChange={handleChange}
+        value={formData.location_name}
+      />
+      <TextField
+        sx={{ mb: 4 }}
+        select
+        label="Currency"
+        name="currency"
+        value={formData.currency}
+        onChange={handleChange}
+      >
+        {currencyList}
+      </TextField>
+      <Button variant="contained" onClick={handleSubmit} disabled={isDisabled}>
+        submit
+      </Button>
+    </FormControl>
+  );
+};
