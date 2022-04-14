6d20da74ec940c2ae1503246577bae6c95ea15e1
Andrew Alagna <aalagna04@gmail.com>
2022-04-14 12:27:38 -0400

    fix: sync all data models locally with DataStore on login
    walk-through was showing up unexpected. Now it will wait for all models to sync before checking if the user needs a walk-through.

--- src/components/Redirect/Redirect.tsx
+++ src/components/Redirect/Redirect.tsx
@@ -3,19 +3,39 @@ import { LocalStorage } from "../../services";
 import { CognitoUser } from "amazon-cognito-identity-js";
 import { useNavigate } from "react-router-dom";
 import { API } from "../../services/api";
+import { Hub, DataStore, Predicates } from "aws-amplify";
 
 export const RedirectComponent = ({ user }: { user: CognitoUser }) => {
   const navigate = useNavigate();
+
   useEffect(() => {
-    LocalStorage.setUser(user);
-    getData();
-  }, []);
+      LocalStorage.setUser(user);
+    // Create listener that will stop observing the model once the sync process is done
+    const removeListener = Hub.listen("datastore", async (capsule) => {
+      const {
+        payload: { event },
+      } = capsule;
+ 
+      // console.log("DataStore event", event, data);
+ 
+      if (event === "ready") {
+        getData();
+      }
+    });
+    // Start the DataStore, this kicks-off the sync process.
+      DataStore.start();
+    return () => {
+      removeListener();
+    };
+  },
+    []);
+
 
   async function getData() {
     const locations = await API.listLocations();
     const business = await API.getBusinessByUsername();
     const promise = (await Promise.all([locations, business])) as any[];
-
+    console.log(promise)
     const isRedirected = promise.includes(null || undefined);
 
     if (isRedirected) return navigate("/user/walkthrough");
