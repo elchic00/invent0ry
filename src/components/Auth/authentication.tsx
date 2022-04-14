fdb4fbaa739e775e86f6c8793650d8f321a0aba1
israx <70438514+israx@users.noreply.github.com>
2022-04-14 09:03:03 -0500

    feat: walkthrough modal

--- src/components/Auth/authentication.tsx
+++ src/components/Auth/authentication.tsx
@@ -1,12 +1,22 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
-import { RedirectComponent } from "../Redirect";
+import { lazy, Suspense } from "react";
+
+const RedirectComponent = lazy(() =>
+  import("../Redirect").then(({ RedirectComponent }) => ({
+    default: RedirectComponent,
+  }))
+);
 
 export const AuthenticatorComp = () => {
   return (
     <Authenticator>
-      {({ signOut, user }) => <RedirectComponent user={user} />}
+      {({ signOut, user }) => (
+        <Suspense fallback={<div>Loading...</div>}>
+          <RedirectComponent user={user} />
+        </Suspense>
+      )}
     </Authenticator>
   );
 };
--- src/pages/WalkThrough/WalkThrough.tsx
+++ src/pages/WalkThrough/WalkThrough.tsx
@@ -1,11 +1,13 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { BusinessForm } from "../../components/BusinessForm";
 import { AddLocation } from "../../components/AddLocation";
 import { AddItem } from "../../components/AddItem";
+import { useNavigate } from "react-router-dom";
+import { CenteredComponent } from "../../components/CenteredComponent";
+import { Modal } from "@mui/material";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -24,11 +26,7 @@ function TabPanel(props: TabPanelProps) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
     </div>
   );
 }
@@ -41,34 +39,43 @@ function a11yProps(index: number) {
 }
 
 export const WalkThrough = () => {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
+  const [value, setValue] = useState(0);
+  const [open, setOpen] = useState(true);
   return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs value={value} aria-label="basic tabs example">
-          <Tab label="Add a location" {...a11yProps(0)} />
-          <Tab label="Add a business" {...a11yProps(1)} />
-          <Tab label="Add an item" {...a11yProps(2)} />
-          <Tab label="Thank you" {...a11yProps(3)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        <AddLocation setValue={setValue} />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <BusinessForm setValue={setValue} />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <AddItem setValue={setValue} />
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        <Box>Thank you</Box>
-      </TabPanel>
-    </Box>
+    <Modal open={open} onClose={() => {}}>
+      <CenteredComponent>
+        <Box sx={{ width: "100%" }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs value={value} aria-label="basic tabs example">
+              <Tab label="Add a location" {...a11yProps(0)} />
+              <Tab label="Add a business" {...a11yProps(1)} />
+              <Tab label="Add an item" {...a11yProps(2)} />
+              <Tab label="Thank you" {...a11yProps(3)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+            <AddLocation setValue={setValue} />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <BusinessForm setValue={setValue} />
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            <AddItem setValue={setValue} />
+          </TabPanel>
+          <TabPanel value={value} index={3}>
+            <ThankYou />
+          </TabPanel>
+        </Box>
+      </CenteredComponent>
+    </Modal>
   );
 };
+
+function ThankYou() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/user/dashboard");
+  }, []);
+
+  return <Box>Thank You</Box>;
+}
