8378b973bffe8103c20770c336621f7a9ec013c1
israx <70438514+israx@users.noreply.github.com>
2022-05-07 17:40:36 -0400

    feat: Dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,77 @@
-import { Box } from "@mui/material";
-import { sendNotification } from "../../utils/sendNotification";
-import { useEffect } from "react";
-import { useItems } from "../../context";
+import { Box, Paper, Skeleton, Typography } from "@mui/material";
+import { useFilterItems } from "../../hooks";
 import { useBusiness } from "../../hooks";
 import { useLocations } from "../../hooks";
+import { FilterItems } from "../../interface/models/enums";
+import FlipCard from "../../components/FlipCard/FlipCard";
 
 export const Dashboard = () => {
   const { locations } = useLocations();
   const { business } = useBusiness();
-  return <Box>Dashboard</Box>;
+  const { filteredItems } = useFilterItems(FilterItems.STOCK);
+
+  console.log({ locations, business, filteredItems });
+  return (
+    <Box>
+      {/*---------------------BUSINESS---------------- */}
+      {business !== null ? (
+        <Typography variant="h3" sx={{ fontWeight: 200, mb: 5 }}>
+          {business?.name}
+        </Typography>
+      ) : (
+        <Skeleton
+          sx={{ width: { xs: 350, sm: 450 }, height: 50 }}
+          variant="rectangular"
+        />
+      )}
+      {/* ----------------------LOCATIONS--------------------- */}
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <Typography variant="h4" sx={{ mb: 1, fontWeight: 100 }}>
+          Locations:
+        </Typography>
+        {locations != null ? (
+          <Box mb={5} sx={{ pl: 3, display: "flex", gap: 2, flexWrap: "wrap" }}>
+            {locations.map((location) => (
+              <Paper
+                elevation={1}
+                key={location.id}
+                sx={{ p: 1, width: "auto" }}
+              >
+                {`${location.street} - ${location.town} - ${location.zip}`}
+              </Paper>
+            ))}
+          </Box>
+        ) : (
+          <Skeleton
+            sx={{ width: { xs: 350, sm: 450 }, height: { xs: 200, sm: 300 } }}
+            variant="rectangular"
+          />
+        )}
+      </Box>
+      {/* ----------------OUT-OF-STOCK-ITEMS---------------- */}
+      <Box>
+        <Typography variant="h4" sx={{ mb: 1, fontWeight: 100 }}>
+          Out of Stock Items:
+        </Typography>
+        <Box sx={{ pl: 3, display: "flex", flexWrap: "wrap", gap: 2 }}>
+          {filteredItems ? (
+            filteredItems.map((item) => (
+              <FlipCard
+                key={item.id}
+                name={item.name}
+                itemCount={item.itemCount}
+                picture={item.picture || ""}
+                expire={item.expire}
+                price={item.price}
+                id={item.id}
+                locationID={item.locationsID}
+              />
+            ))
+          ) : (
+            <Skeleton variant="rectangular" width={500} height={118} />
+          )}
+        </Box>
+      </Box>
+    </Box>
+  );
 };
