c299a71ea41293677fc20f657cd018c4718371c9
israx <70438514+israx@users.noreply.github.com>
2022-05-16 10:51:14 -0400

    feat: useCount

---
+++ src/hooks/useCount.ts
@@ -0,0 +1,37 @@
+import { getContainerUtilityClass } from "@mui/material";
+import { resolve } from "path";
+import { useCallback, useState, useEffect } from "react";
+import { useItems } from "../context";
+import { Items } from "../models";
+
+type countType = {
+  quantity: number;
+  assets: number;
+};
+export const useCount = () => {
+  const [count, setCount] = useState<countType>({ quantity: 0, assets: 0.0 });
+  const { items } = useItems();
+
+  useEffect(() => {
+    getCount();
+  }, [items]);
+
+  function getCount() {
+    const reducedItems = items?.reduce(handleReduce, {
+      quantity: 0,
+      assets: 0.0,
+    });
+    reducedItems.assets = reducedItems.assets.toFixed(2);
+    setCount(reducedItems);
+    return new Promise((resolve, reject) => {
+      resolve(count);
+    });
+  }
+
+  function handleReduce(prev: any, current: any) {
+    prev.quantity = current.itemCount + prev.quantity;
+    prev.assets = current.itemCount * current.price + prev.assets;
+    return prev;
+  }
+  return { count };
+};
